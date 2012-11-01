e07cd0d0afb4783f47496ea69d8e536f0dc8c2f7
chavp <my.parinya@gmail.com>
2012-11-01 18:00:01 +0700

    first commit
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ MVVM_ASP_MVC_WEB/Controllers/CustomersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVVM_ASP_MVC_WEB.Controllers
+{
+    public class CustomersController : Controller
+    {
+        //
+        // GET: /Customers/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVVM_ASP_MVC_WEB/Global.asax.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace MVVM_ASP_MVC_WEB
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+
+            BundleTable.Bundles.RegisterTemplateBundles();
+            BundleTable.Bundles.EnableBootstrapBundle();
+        }
+
+    }
+
+    public static class BundleTableExtension
+    {
+        public static void EnableBootstrapBundle(this BundleCollection bundles)
+        {
+            //var flexigridCss = new Bundle("~/flexigrid/css", new CssMinify());
+            //flexigridCss.AddFile("~/Scripts/flexigrid/css/flexigrid.css");
+            //bundles.Add(flexigridCss);
+
+            var bootstrapJs = new Bundle("~/bootstrap/js", new JsMinify());
+            bootstrapJs.AddFile("~/Scripts/knockout-2.0.0.js");
+            //bootstrapJs.AddFile("~/Scripts/flexigrid/js/flexigrid.js");
+
+            bundles.Add(bootstrapJs);
+        }
+    }
+}
